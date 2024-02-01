8380f4fbeb7bb150dc459e027bed46d22b51a28a
gromedev <61883549+gromedev@users.noreply.github.com>
2024-02-01 12:15:43 +0100

    Dev branch

--- .old/Public/AzureAD/Invoke-ForceIntuneSync.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.SYNOPSIS
-This function retrieves the 'PushLaunch' scheduled task using the Get-ScheduledTask cmdlet and starts the task using the Start-ScheduledTask cmdlet. The 'PushLaunch' task is a scheduled task that is typically used to synchronize with the Intune service. Starting this task initiates a synchronization with Intune, allowing changes made in the Intune console to be reflected on the device.
-#>
-function Invoke-ForceIntuneSync() {
-    Write-Output "`nInvoke-ForceIntuneSync"
-    Get-ScheduledTask | Where-Object { $_.TaskName -eq "PushLaunch" } | Start-ScheduledTask
-}
--- .old/Public/Module/Confirm-Winget.ps1
+++
@@ -1,8 +0,0 @@
-function Confirm-Winget {
-    # Install winget if it is not already installed
-    if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
-        Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v0.2.2224-beta/winget-cli-0.2.2224-beta-windows-x64.zip -OutFile winget.zip
-        Expand-Archive -Path winget.zip -DestinationPath .
-        winget.exe install winget
-    }
-}
